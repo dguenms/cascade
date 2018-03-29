98afbc707eca42185c1be5a231594f9b906ccf7b
Daniel Gunther <daniel_gunther@intuit.com>
2018-03-29 11:21:08 +0200

    Additional tests for creating steps from definitions.

--- parser/steps_test.go
+++ parser/steps_test.go
@@ -45,3 +45,46 @@ func TestCreatesMultipleBuildStepsFromBuildStepsDefinition(t *testing.T) {
 
     require.Equal(t, expectedBuildSteps, buildSteps)
 }
+
+func TestResolvesNextSteps(t *testing.T) {
+    stepDef2 := BuildStepDef{"second-repo", "second-path", "second command", []string{"first_step"}, []string{"second-path"}}
+    stepsDef := BuildStepsDef{"first_step": stepDef, "second_step": stepDef2}
+
+    buildSteps := stepsDef.create()
+
+    expectedBuildStep := BuildStep{"some-repo", "some-path", "some command", []string(nil), []string{"some-path"}, []string{"second_step"}}
+    expectedBuildStep2 := BuildStep{"second-repo", "second-path", "second command", []string{"first_step"}, []string{"second-path"}, []string(nil)}
+    expectedBuildSteps := BuildSteps{"first_step": expectedBuildStep, "second_step": expectedBuildStep2}
+
+    require.Equal(t, expectedBuildSteps, buildSteps)
+}
+
+func TestResolvesMultipleNextSteps(t *testing.T) {
+    stepDef2 := BuildStepDef{"second-repo", "second-path", "second command", []string{"first_step"}, []string(nil)}
+    stepDef3 := BuildStepDef{"third-repo", "third-path", "third command", []string{"first_step"}, []string(nil)}
+    stepsDef := BuildStepsDef{"first_step": stepDef, "second_step": stepDef2, "third_step": stepDef3}
+
+    buildSteps := stepsDef.create()
+
+    expectedBuildStep := BuildStep{"some-repo", "some-path", "some command", []string(nil), []string{"some-path"}, []string{"second_step", "third_step"}}
+    expectedBuildStep2 := BuildStep{"second-repo", "second-path", "second command", []string{"first_step"}, []string(nil), []string(nil)}
+    expectedBuildStep3 := BuildStep{"third-repo", "third-path", "third command", []string{"first_step"}, []string(nil), []string(nil)}
+    expectedBuildSteps := BuildSteps{"first_step": expectedBuildStep, "second_step": expectedBuildStep2, "third_step": expectedBuildStep3}
+
+    require.Equal(t, expectedBuildSteps, buildSteps)
+}
+
+func TestNoDuplicateNextSteps(t *testing.T) {
+    stepDef2 := BuildStepDef{"second-repo", "second-path", "second command", []string{"first_step"}, []string(nil)}
+    stepDef3 := BuildStepDef{"third-repo", "third-path", "third command", []string{"first_step"}, []string(nil)}
+    stepsDef := BuildStepsDef{"first_step": stepDef, "second_step": stepDef2, "third_step": stepDef3}
+
+    buildSteps := stepsDef.create()
+
+    expectedBuildStep := BuildStep{"some-repo", "some-path", "some command", []string(nil), []string{"some-path"}, []string{"second_step", "third_step"}}
+    expectedBuildStep2 := BuildStep{"second-repo", "second-path", "second command", []string{"first_step"}, []string(nil), []string(nil)}
+    expectedBuildStep3 := BuildStep{"third-repo", "third-path", "third command", []string{"first_step"}, []string(nil), []string(nil)}
+    expectedBuildSteps := BuildSteps{"first_step": expectedBuildStep, "second_step": expectedBuildStep2, "third_step": expectedBuildStep3}
+
+    require.Equal(t, expectedBuildSteps, buildSteps)
+}
